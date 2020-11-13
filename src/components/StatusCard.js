2d4de3d3ae0bd58e7edbaf40b26f4a2f1add4a51
MgSaiNyiNyiTun <56770099+mgsainyinyitun@users.noreply.github.com>
2020-11-13 17:27:27 +0630

    modify something

--- src/components/StatusCard.js
+++ src/components/StatusCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './StatusCard.css';
 
 const StatusCard = (props) =>{
-    const velocity = (2/props.speed)*1000;
+    const velocity = Math.floor((2/props.speed)*1000);
     return(
         <div className="status-card">
-            <h5>LENGTH :: {props.length} </h5>
-            <h5>SPEED :: {velocity} pixel / sec</h5>
+            <p className="info">LENGTH :: {props.length} </p>
+            <p className="info">SPEED :: {velocity} pixel / sec</p>
         </div>
     )
 }
